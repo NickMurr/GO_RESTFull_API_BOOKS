17d03c5b32a0a2776370aea6c18398a8ffd6eb09
nickmurr <32221055+NickMurr@users.noreply.github.com>
2019-06-05 19:04:45 +0300

    15 - Implementing 'addBook' to add a book record to the table

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
-	"github.com/lib/pq"
-
-	"github.com/subosito/gotenv"
-
 	"github.com/gorilla/mux"
+	"github.com/lib/pq"
+	"github.com/subosito/gotenv"
 )
 
 // Book structure
@@ -36,9 +35,8 @@ func logFatal(err error) {
 
 func main() {
 	pgURL, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
-	logFatal(err)
 
-	db, err := sql.Open("postgres", pgURL)
+	db, err = sql.Open("postgres", pgURL)
 	logFatal(err)
 
 	err = db.Ping()
@@ -56,12 +54,48 @@ func main() {
 }
 
 func getBooks(w http.ResponseWriter, r *http.Request) {
+	var book Book
+
+	books = []Book{}
+
+	rows, err := db.Query("select * from books;")
+	logFatal(err)
+
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+		logFatal(err)
+
+		books = append(books, book)
+	}
+
+	json.NewEncoder(w).Encode(books)
+
 }
 
 func getBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	params := mux.Vars(r)
+
+	rows := db.QueryRow("select * from books where id=$1", params["id"])
+
+	err := rows.Scan(&book.ID, &book.Title, &book.Author, &book.Year)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(book)
 }
 
 func addBook(w http.ResponseWriter, r *http.Request) {
+	var book Book
+	var bookID int
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	err := db.QueryRow("insert into books (title, author, year) values($1, $2, $3) RETURNING id;", book.Title, book.Author, book.Year).Scan(&bookID)
+	logFatal(err)
+
+	json.NewEncoder(w).Encode(bookID)
 
 }
 
